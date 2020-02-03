245ba6df6e1a7a6e3642846bbb7af744c31275dd
Greg <g.rutherford10@gmail.com>
2020-02-03 15:04:34 +0000

    add basic input & button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import SearchBox from './components/SearchBox';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Greg Rutherford</h2>
+        <h2>The Big Movie Database</h2>
       </header>
       <main>
+        <SearchBox />
+        <Button />
         <p>Add Content here ...</p>
       </main>
     </div>
---
+++ src/components/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchBox.css';
+
+const SearchBox = () => (
+  <>
+    <input type="text"></input>
+  </>
+);
+
+export default SearchBox;
